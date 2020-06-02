2c073c2e3a34daef29935b57b9f11e04019c2802
Fabio Vinteira <fabiovinteira@gmail.com>
2020-06-02 01:22:26 -0300

    Primeiro commit, classes e metodos basicos retornando

---
+++ Poli.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Poli
+{
+    public static class Circ
+    {
+        public static double Area(double raio)
+        {
+            return Math.PI * Math.Pow(2*raio, 2);
+        }
+
+        public static double Perimetro(double raio)
+        {
+            return Math.PI * (2 * raio);
+        }
+
+        public static double Diametro(double raio)
+        {
+            return raio * 2;
+        }
+    }
+
+    public class Quad
+    {
+        public static double Area(double lado)
+        {
+            return Math.Pow(lado, 2);
+        }
+
+        public static double Perimetro(double lado)
+        {
+            return lado * 4;
+        }
+
+        public static double Diag(double lado)
+        {
+            return Math.Sqrt(Math.Pow(lado, 2) + Math.Pow(lado, 2));
+        }
+    }
+
+    public class Ret
+    {
+        public static double Area(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double Perimetro(double n1, double n2)
+        {
+            return 2 * n1 + 2 * n2;
+        }
+
+        public static double Diag(double n1, double n2)
+        {
+            return Math.Sqrt(Math.Pow(n1, 2) + Math.Pow(n2, 2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poligonos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.Write("Digite o raio de um circulo: ");
+            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Para um Circulo de raio: " + raio.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area: " + Poli.Circ.Area(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + Poli.Circ.Perimetro(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diametro: " + Poli.Circ.Diametro(raio).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("");
+            Console.Write("Digite o lado de um quadrado: ");
+            double lado = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Para um Quadrado de lado: " + lado.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area: " + Poli.Quad.Area(lado).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + Poli.Quad.Perimetro(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + Poli.Quad.Diag(raio).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("");
+            Console.Write("Digite os lados de um retangulo (2): ");
+            string[] lr = Console.ReadLine().Split(' ');
+            double l1 = double.Parse(lr[0]);
+            double l2 = double.Parse(lr[1]);
+
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Para um Retangulo de lados: " + l1.ToString("F2", CultureInfo.InvariantCulture) + " e " + l2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area: " + Poli.Ret.Area(l1, l2).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + Poli.Ret.Perimetro(l1, l2).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + Poli.Ret.Diag(l1, l2).ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
